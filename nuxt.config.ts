0bfd2592eabb37121ff9d3e2a2c547ecc78e90d9
jwar28 <javierguerra2203@gmail.com>
2023-03-11 21:56:32 -0500

    .env corrections

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
   srcDir: "src",
   runtimeConfig: {
-    public: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    },
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
 });
